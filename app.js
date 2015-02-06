12bab328063d646e92ece7f39188f5823fcaf3ed
Andrew Daniels <andrew.daniels714@gmail.com>
2015-02-06 17:55:52 -0500

    Implement scoring algorithm

--- app.js
+++ app.js
@@ -4,16 +4,32 @@ $(document).ready(function() {
         $('.quiz').hide();
         // $('.quiz').slideUp("fast", function() {});
 
+        var scores = [0, 0, 0, 0, 0, 0, 0 ,0]
+
         $('.option input').each(function(){
             if (this.checked) {
-                console.log(this.value);
+                if(this.value.indexOf("ba") >= 0){
+                    scores[0]++;
+                } else if(this.value.indexOf("r") >= 0) {
+                    scores[1]++;
+                } else if(this.value.indexOf("l") >= 0) {
+                    scores[2]++;
+                } else if(this.value.indexOf("ho") >= 0) {
+                    scores[3]++;
+                } else if(this.value.indexOf("m") >= 0) {
+                    scores[4]++;
+                } else if(this.value.indexOf("he") >= 0) {
+                    scores[5]++;
+                } else if(this.value.indexOf("bu") >= 0) {
+                    scores[6]++;
+                } else if(this.value.indexOf("q") >= 0) {
+                    scores[7]++;
+                }
             };
-
         });
-        
 
         var houses = ["Baxter", "Reed", "Ladd", "Howell", "MacMillan", "Helmreich", "Burnett", "Quinby"];
-        var i = Math.floor(houses.length * Math.random());
+        var i = scores.indexOf(Math.max.apply(Math, scores));
         var house = houses[i];
         $('.house').text(house + " House");
 
